1364ca396e4f3653f8422d3a40e4dcd340a6ae3c
Jan Eglinger <jan.eglinger@fmi.ch>
2024-04-09 21:53:40 +0200

    Exclude irrelevant platform files from copying

--- conda.recipe/build.bat
+++ conda.recipe/build.bat
@@ -2,6 +2,10 @@
 "./fiji/ImageJ-win64.exe" --update update-force-pristine
 @REM Required to avoid '[ERROR] Could not delete ...'
 "./fiji/ImageJ-win64.exe" --update update
-cp -r fiji/* %PREFIX%
+@REM cp -r fiji/* %PREFIX%
+echo linux64 > exclude.txt
+echo macosx >> exclude.txt
+echo win32 >> exclude.txt
+xcopy fiji\* %PREFIX% /s /exclude:exclude.txt
 @REM TODO: add more update sites by calling the updater
 @REM TODO: remove platform-specific files when building for each platform?
--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,5 +1,6 @@
 ./fiji/ImageJ-linux64 --update add-update-site MoBIE https://sites.imagej.net/MoBIE/
 ./fiji/ImageJ-linux64 --update update-force-pristine
-cp -r fiji/* $PREFIX
+# cp -r fiji/* $PREFIX
+rsync -a --exclude win64 --exclude win32 --exclude macosx fiji/* $PREFIX
 # TODO: add more update sites by calling the updater
 # TODO: remove platform-specific files when building for each platform?
